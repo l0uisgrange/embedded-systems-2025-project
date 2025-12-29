575ed8fbb9f5475634e185bd112e7a9d44a9ab55
Louis Grange <70532216+l0uisgrange@users.noreply.github.com>
2025-12-29 12:00:49 +0100

    Clean up main.h by removing unused code
    Removed unnecessary comments and RGB LED definitions.

--- src/main.h
+++ src/main.h
@@ -5,16 +5,8 @@
 extern "C" {
 #endif
 
-/* The following includes and declarations are necessary as this main overrides the main of the library,
- and some files in the library require these includes/declarations to be in the main.h. */
-
-#include <camera/dcmi_camera.h>
 #include <msgbus/messagebus.h>
 #include <parameter/parameter.h>
-
-//RGB LED used for interaction with plotImage Python code
-#define USED_RGB_LED            LED4
-#define INTENSITY_RGB_LED       10
 
 /** Robot wide IPC bus. */
 extern messagebus_t bus;
